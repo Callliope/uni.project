6809cf92969f004b277a31527c6b0100617d25f8
Maria Mihailovskaya <mihailovskayam33@gmail.com>
2022-06-12 12:42:36 +0300

    add gitignore files

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ function showinput(sender) {
     if (math.checked) {
         document.getElementById("mathblock").style.display = "block";
     }
+    if (informatic.checked) {
+        document.getElementById("infoblock").style.display = "block";
+    }
+}
+function Check(){
+        if (document.getElementById("city").value =='')
+        {
+                alert('Введите значение в поле "Город"!');
+                return false;
+        }
 }
